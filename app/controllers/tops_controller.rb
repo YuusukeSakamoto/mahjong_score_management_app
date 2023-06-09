4957f09b3644157721ba9d2f22be3d9f1461090a
EC2 Default User <ec2-user@ip-172-31-19-107.ap-northeast-1.compute.internal>
2023-06-09 14:21:11 +0000

    commit

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,6 +1,9 @@
 class TopsController < ApplicationController
   
   def show
-    
+    if user_signed_in?
+      match_ids = Result.where(player_id: current_user.player.id).select(:match_id).pluck(:match_id)
+      @matches = Match.where(id: match_ids).order(match_on: :DESC).first(5)
+    end
   end
 end
