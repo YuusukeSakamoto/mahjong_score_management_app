2fd00fcbfaf21814fef168ae4d0cd9f30394d44d
YuusukeSakamoto <124509500+YuusukeSakamoto@users.noreply.github.com>
2024-01-13 21:34:24 +0900

    fix: メーラーの設定を修正

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -3,6 +3,6 @@
 class ContactMailer < ApplicationMailer
   def contact_mail(contact, user)
     @contact = contact
-    mail to: user.email, bcc: ENV['ACTION_MAILER_USER'], subject: 'お問い合わせについて【自動送信】'
+    mail to: user.email, bcc: ENV['SEND_MAIL'], subject: 'お問い合わせについて【自動送信】'
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -49,8 +49,8 @@ Rails.application.configure do
 
   # メーラーの設定(2023/10/22 追記)
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :letter_opener_web
-  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.delivery_method = :letter_opener_web
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                  587,
@@ -60,7 +60,6 @@ Rails.application.configure do
     authentication:       'plain',
     enable_starttls_auto:  true
   }
-
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -98,6 +97,6 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
   config.hosts.clear
-  
-  Rails.application.routes.default_url_options[:host] = 'http://localhost:3000/'
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
