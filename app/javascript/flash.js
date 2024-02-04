4daec7cec72e716e8bb3525ae830f3ec132c9098
YuusukeSakamoto <124509500+YuusukeSakamoto@users.noreply.github.com>
2024-02-04 21:44:34 +0900

    fix: フラッシュメッセージを3秒後にフェードアウトする要修正

--- app/javascript/flash.js
+++ app/javascript/flash.js
@@ -1,7 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
-      // 閉じるボタンのクリックイベント
-    $(document).on('click', '.close-flash', function() {
-      $(this).parent().fadeOut(); // 親要素（Flashメッセージ）をフェードアウト
-    });
+    // 閉じるボタンのクリックイベント
+  $(document).on('click', '.close-flash', function() {
+    $(this).parent().fadeOut(); // 親要素（Flashメッセージ）をフェードアウト
+  });
+
+  // Flashメッセージを3秒後に自動的にフェードアウト
+  setTimeout(function() {
+      $('.close-flash').parent().fadeOut();
+  }, 3000); // 3000ミリ秒 = 3秒
 
 });
