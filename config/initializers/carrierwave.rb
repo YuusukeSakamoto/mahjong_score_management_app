9e77d221d6e03a27c43894cbf553cc6924d77481
YuusukeSakamoto <124509500+YuusukeSakamoto@users.noreply.github.com>
2024-02-12 21:27:05 +0900

    fix: 不要な処理削除

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,7 +4,6 @@ require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
     config.storage :fog
-    config.fog_provider = 'fog/aws'
     config.fog_directory  = 'janreco-s3' # 作成したバケット名
     config.fog_credentials = {
       provider: 'AWS',
