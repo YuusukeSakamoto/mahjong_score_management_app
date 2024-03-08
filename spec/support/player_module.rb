b7d030857a050f11afffcef007504c5dcb917e8f
YuusukeSakamoto <124509500+YuusukeSakamoto@users.noreply.github.com>
2024-03-08 23:56:52 +0900

    test: クリック操作改善

--- spec/support/player_module.rb
+++ spec/support/player_module.rb
@@ -1,10 +1,11 @@
 module PlayerModule
   # プレイヤー選択
   def create_players(players)
-  visit new_player_path(play_type: 4)
-  find('li', text: players[0].name).click
-  find('li', text: players[1].name).click
-  find('li', text: players[2].name).click
-  find('#create_btn', visible: true).click # プレイヤー作成ボタンをクリック
+    visit new_player_path(play_type: 4)
+    find('li', text: players[0].name, visible: :all).click
+    find('li', text: players[1].name, visible: :all).click
+    element = find('li', text: players[2].name, visible: :all)
+    page.execute_script('arguments[0].click();', element.native)
+    find('#create_btn', visible: true).click # プレイヤー作成ボタンをクリック
   end
 end
